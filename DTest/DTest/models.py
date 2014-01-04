a87b057d35417a8cb8d31349086eba19498a0896
twok <scvdermody@gmail.com>
2014-01-03 16:09:00 -0800

    Update models.py
    Minor formatting and rewrite. Bug testing starts tonight/tomorrow, but this should be a tighter approach with a bit less overhead.

--- DTest/DTest/models.py
+++ DTest/DTest/models.py
@@ -1,26 +1,22 @@
 from django.db import models
 
-def getTorrent(hash):
-    for t in Torrent.objects.all():
-        if hash.lower() == t.info_hash.lower():
-            # d,k = **t # Check this!!!
-            peers = {}
-            return {"name":t.name,
-                    "info_hash":t.info_hash,
-                    "size":t.size,
-                    "peers":peers,
-                    "torrent":t
-                   }
-#    print "Suspected problem spot"
-    raise Exception("Torrent not found: - info_hash: {} - passedhash: {}".format(hash.encode('utf-8')
-                                                                                ,t.info_hash))
-
-
+###################
+# Class - Client  #
+###################
 
 class Client(models.Model):
+    """Instances represent a peer entity in the torrent swarm.
+       These should be very malleable; for the time being, until
+       a better solution is provided. Note that it is one Client
+       per torrent, not one client with many torrents.
+       >>> c = Client.create(request)
+       >>> if c.is_valid() c.save()
+    """
+    
     name = models.CharField(max_length=20)
     ip = models.GenericIPAddressField()
     port = models.IntegerField()
+    torrent = models.ForeignKey(Torrent)
 
     state_choices = (
         ('seeding','Seeding'),
@@ -32,6 +28,11 @@ class Client(models.Model):
 
 
     state = models.CharField(max_length=12,choices=state_choices, default='inactive')
+    
+    @classmethod
+    def create(request):
+        c = Client(name="Test",ip="192.168.1.1",port=2489)
+        return c
 
     def __unicode__(self):
         return self.name
@@ -44,13 +45,14 @@ class Client(models.Model):
         self.state = options[state.lower()]()
         
 
-    def __startSeeding__(self):
+    def __startSeeding__(self): # Little more than a "flag" for identifying prime peers
         return "Seeding"
 
-    def __initialize__(self):
+    def __initialize__(self): # Add / create client
         return "Starting"
 
-    def __quit__(self):
+    def __quit__(self): # Attempt to remove the client entity
+        self.delete()
         return "Inactive"
             
 
@@ -58,25 +60,36 @@ class Torrent(models.Model):
     name = models.CharField(max_length=200,unique=True)
     info_hash = models.CharField(max_length=20)
     size = models.BigIntegerField()
-    peers = models.ManyToManyField(Client)
 
+    @classmethod
+    def create(info_hash=None):
+        pass
+        # return Torrent.get_or_create()
+    
     def __unicode__(self):
         return self.name
 
-    def getPeers(self):
-        p = {}
-        for peer in self.peers.all():
-            p[peer.name] = peer,(True if self in peer.getSeeds() else False)
-        print p
 
 
 
+#####################
+# Utility functions #
+#####################
 
-
-#############
-# Utility functions
-#############
-
+def getTorrent(hash):
+    for t in Torrent.objects.all():
+        if hash.lower() == t.info_hash.lower():
+            # d,k = **t # Check this!!!
+            peers = {}
+            return {"name":t.name,
+                    "info_hash":t.info_hash,
+                    "size":t.size,
+                    "peers":t.client_set.all(),
+                    "torrent":t
+                   }
+#    print "Suspected problem spot"
+    raise Exception("Torrent not found:\ninfo_hash: {}\npassedhash: {}".format(hash.encode('utf-8')
+                                                                                ,t.info_hash))
 
 #############
 # Database read/write
