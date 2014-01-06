60c41b4495ce3a0d34a998e8da7d7183540c534d
twok <scvdermody@gmail.com>
2014-01-06 11:11:35 -0800

    Update models.py

--- DTest/DTest/models.py
+++ DTest/DTest/models.py
@@ -1,8 +1,52 @@
 from django.db import models
 
-###################
-# Class - Client  #
-###################
+####################
+# Class - Torrent  #
+####################
+
+# TODO
+# 
+class Torrent(models.Model):
+    """Notes and things
+    """
+    
+    name = models.CharField(max_length=200,unique=True)
+    info_hash = models.CharField(max_length=20,unique=True)
+    size = models.BigIntegerField()
+    
+    def getPeer(name=None):
+        if name is not None:
+            return this.client_set.filter(client_name__startswith=name)
+        else:
+            return this.client_set.all()
+
+    @classmethod
+    def create(info_hash=None,name=None):
+        # Attempt to create and save torrent
+        # If pass, return torrent
+        # If fail, raise exception
+        pass
+    
+    @classmethod
+    def getTorrent(info_hash=None,name=None):
+        for t in Torrent.objects.all():
+            if info_hash.lower() == t.info_hash.lower() or name.lower() == t.name.lower():
+                return {"name":t.name,
+                        "info_hash":t.info_hash,
+                        "size":t.size,
+                        "torrent":t
+                       }
+    #    print "Suspected problem spot"
+        raise Exception("Torrent not found:\ninfo_hash: {}\npassedhash: {}".format(hash.encode('utf-8')
+                                                                                    ,t.info_hash))
+    
+    def __unicode__(self):
+        return self.name
+
+
+##################
+# Class - Client #
+##################
 
 class Client(models.Model):
     """Instances represent a peer entity in the torrent swarm.
@@ -13,7 +57,7 @@ class Client(models.Model):
        >>> if c.is_valid() c.save()
     """
     
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
     ip = models.GenericIPAddressField()
     port = models.IntegerField()
     torrent = models.ForeignKey(Torrent)
@@ -30,8 +74,15 @@ class Client(models.Model):
     state = models.CharField(max_length=12,choices=state_choices, default='inactive')
     
     @classmethod
-    def create(request):
-        c = Client(name="Test",ip="192.168.1.1",port=2489)
+    def create(n = None, i = None, p = None, ih = None):
+        try:
+            t = Torrent.getTorrent(info_hash=ih)["torrent"]
+            c = Client(name=n,ip=i,port=p, torrent=t)
+            c.save()
+        except NameError:
+            print "One or more variables may not have been initialized."
+        except Exception as e:
+            print e
         return c
 
     def __unicode__(self):
@@ -54,59 +105,3 @@ class Client(models.Model):
     def __quit__(self): # Attempt to remove the client entity
         self.delete()
         return "Inactive"
-            
-
-class Torrent(models.Model):
-    name = models.CharField(max_length=200,unique=True)
-    info_hash = models.CharField(max_length=20)
-    size = models.BigIntegerField()
-
-    @classmethod
-    def create(info_hash=None):
-        pass
-        # return Torrent.get_or_create()
-    
-    def __unicode__(self):
-        return self.name
-
-
-
-
-#####################
-# Utility functions #
-#####################
-
-def getTorrent(hash):
-    for t in Torrent.objects.all():
-        if hash.lower() == t.info_hash.lower():
-            # d,k = **t # Check this!!!
-            peers = {}
-            return {"name":t.name,
-                    "info_hash":t.info_hash,
-                    "size":t.size,
-                    "peers":t.client_set.all(),
-                    "torrent":t
-                   }
-#    print "Suspected problem spot"
-    raise Exception("Torrent not found:\ninfo_hash: {}\npassedhash: {}".format(hash.encode('utf-8')
-                                                                                ,t.info_hash))
-
-#############
-# Database read/write
-#############
-
-def handleClient(info=None,req=None,torrent=None):
-    try:
-        # Try to fetch client
-        c, created = Client.objects.get_or_create(name=info[u'peer_id'],
-                                                          ip=req.META["REMOTE_ADDR"],
-                                                          port=int(info[u'port'][0]),
-                                                          )
-        c.setState(info[u'event'][0])
-        # Later; check params for downloaded / left, try to use peers w/ less
-        # data remaining to pass to lacking peers, when seeders are unavailable
-        #
-        # Save client changes
-    except Exception as e:
-        print e, "<--Here?"
-    print "Probs not here."
