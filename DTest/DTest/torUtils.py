20d86652aab013f3168929aa5c0a00faeabc6ea6
twok <scvdermody@gmail.com>
2014-01-06 09:15:50 -0800

    Cleaning up torUtils.
    Considering integrating into views, but depends on how this class gets updated in the future.

--- DTest/DTest/torUtils.py
+++ DTest/DTest/torUtils.py
@@ -2,59 +2,13 @@ import binascii
 import bencode
 import hashlib
 import urllib
-####
-# torrent-protocol
-########
-
-class torUtils:
-
-	# Generic bencoder.
-	def bencode(self, o):
-		string = ""
-
-		if type(o) is list:
-			string = self.__bencode_list(o)
-		elif type(o) is dict:
-			string = self.__bencode_dict(o)
-		elif type(o) is str:
-			string = self.__bencode_str(o)
-		elif type(o) is int:
-			string = self.__bencode_int(o)
-		else:
-			raise ValueError('Not a list, dict, str, or int')
-		return string
-
-	def __bencode_list(self, o):
-		temp = "l"
-		for i in o:
-			temp += self.bencode(i)
-		temp += "e"
-		return temp
-
-	def __bencode_dict(self, o):
-		temp = "d"
-		for k in o.keys():
-			temp += self.bencode(k)
-			temp += self.bencode(o[k]) # hahaha
-		temp += "e"
-		return temp
-
-	def __bencode_str(self, o):
-		temp = str(len(o)) + ":"
-		temp += o
-		return temp
-
-	def __bencode_int(self, o):
-		temp = "i" + str(o) + "e"
-		return temp
-
-
-	# Generic bdecoder here
 
 #############################
 # Generic utility functions #
 #############################
 
+# TODO
+# Clean up a bit, but otherwise ok for now
 def getParams(url):
     """getParams(<string>):
         returns a dictionary of an & delimited url, with its respective keys and values
@@ -68,14 +22,18 @@ def getParams(url):
         d[k.split("=")[0]]=k.split("=")[1:]
     return d
 
+# TODO
+# Error-checking
 def getHash(file): # ex getHash(open("file.torrent").read())
     """Return the info_hash for the particular torrent.
        Should probably take a shot at adding error-checking
        here."""
     hash = bencode.bdecode(file)
-    hash1 = hashlib.sha1(bencode.bencode(hash['info'])).hexdigest()
-    return binascii.a2b_hex(hash1)
+    hash = hashlib.sha1(bencode.bencode(hash['info'])).hexdigest()
+    return binascii.a2b_hex(hash)
 
+# TODO
+# Flesh out the dictionary; this will be sent to clients (likely)
 def getInfo(file,name):
     return {"name":name,
             "info_hash":urllib.quote(getHash(file)),
